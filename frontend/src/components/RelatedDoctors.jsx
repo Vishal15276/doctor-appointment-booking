d189c16572fd07a14a83465e5d043a8edd144eda
Vishal15276 <126046094+Vishal15276@users.noreply.github.com>
2025-01-28 16:23:29 +0530

    REACT

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,18 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
-import { AppContext } from '../context/AppContext';
 
-const TopDoctors = () => {
-const navigate=useNavigate();
-const {doctors}=useContext(AppContext)
+const RelatedDoctors = ({speciality,docId}) => {
+
+    const {doctors}=useContext(AppContext)
+    const navigate=useNavigate()
+
+    const [relDoc,setRelDocs]=useState([])
+
+    useEffect(()=>{
+        if(doctors.length>0 && speciality){
+            const doctorsData=doctors.filter((doc)=>doc.speciality===speciality && doc._id!==docId)
+            setRelDocs(doctorsData)
+        }
+    },[doctors,speciality,docId])
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
       <div className='w-full grid grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {doctors.slice(0,10).map((item, index) => (
-          <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}}key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 w-full max-w-md'>
+        {relDoc.slice(0,5).map((item, index) => (
+          <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 w-full max-w-md'>
             <img className='bg-blue-50' src={item.image} alt="" />
             <div className='p-4'>
               <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -29,4 +39,4 @@ const {doctors}=useContext(AppContext)
   )
 }
 
-export default TopDoctors
+export default RelatedDoctors
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <div className='text-center text-2xl pt-10 text-gray-6=500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb</p>
+          <p>fuewjb koj b wufbweoilufb woifbn ewiofh weniofnewion iowenfieg wnifniewfn iwenifn inipn pinpopo i n  lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>euoifdhb ioh eiowh fniowh ioh iohiehfiasfh ksl;fnsakcvnvidonfr iewn fkfdnfiorew lorem100esfcehduisag fuwgfuowdfghb uio weoifhewofih iowefhewoifh ihiwehjfiefhewipfhwe ihjiwo hfewiohfo uwhwu bfwuejkfb  woeufgb</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'> CHOOSE US</span></p>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>EFFICIENCY:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle</p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+        <b>CONVENIENCE:</b>
+        <p>Access to a network of trusted healthcare professionals in your area. </p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+        <b>PERSONALIZATION:</b>
+        <p>Tailored recommendation and reminders to help you stay on top of your health</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
